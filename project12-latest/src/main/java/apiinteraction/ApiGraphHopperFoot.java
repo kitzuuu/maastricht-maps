b66817eff547f79aa8b64ba944e1d46589714a38
Toma Cristian Nitu <65200697+kitzuuu@users.noreply.github.com>
2026-09-14 19:10:31 +0000

    Read GraphHopper API key from an environment variable

--- project12-latest/src/main/java/apiinteraction/ApiGraphHopperFoot.java
+++ project12-latest/src/main/java/apiinteraction/ApiGraphHopperFoot.java
@@ -12,7 +12,7 @@ import java.util.Arrays;
  */
 public class ApiGraphHopperFoot {
     // The API key for the GraphHopper API
-    private static final String KEY = "bf3e0c54-ef50-4c66-9e2c-ab0e70ad4868";
+    private static final String KEY = System.getenv("GRAPHHOPPER_API_KEY");
 
     /**
      * Makes a GET request to the GraphHopper API to get the walking route between
